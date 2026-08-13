8abaf9bc3151549f3a59275a9528706a270b09b0
Wiktor Wijata <wiktorwijata@gmail.com>
2026-08-13 10:01:57 +0200

    DI fix

---
+++ Source/RescuePC.Software.Caching/ServiceCollectionExtensions.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using RescuePC.Software.Caching.Providers.Redis;
+using StackExchange.Redis;
+
+namespace RescuePC.Software.Caching;
+
+public static class ServiceCollectionExtensions
+{
+    public static void AddRedisAsDefaultCacheProvider(this IServiceCollection services, RedisCacheSettings settings)
+    {
+        var configurationOptions = ConfigurationOptions.Parse(settings.ConnectionString);
+
+        if (!string.IsNullOrEmpty(settings.Password))
+        {
+            configurationOptions.Password = settings.Password;
+        }
+
+        services.AddSingleton<IConnectionMultiplexer>(_ => ConnectionMultiplexer.Connect(configurationOptions));
+        services.AddSingleton(settings);
+        services.AddSingleton<ICacheProvider, RedisCacheProvider>();
+    }
+}
